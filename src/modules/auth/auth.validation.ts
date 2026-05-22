85228a5bfaddbbae3398d1babb1ce166cf983d43
hayder <hayderbd4290@gmail.com>
2026-05-23 01:57:02 +0600

    create user auth validation more validate

--- src/modules/auth/auth.validation.ts
+++ src/modules/auth/auth.validation.ts
@@ -1,21 +1,25 @@
 import type { NextFunction, Request, Response } from "express";
-import type { IUserPayload } from "./auth.interface";
+import type { IUserPayload, Roles } from "./auth.interface";
 import { StatusCodes } from "http-status-codes";
 import { AppError } from "../../errors/appError";
 
 const validateUser = (req: Request, res: Response, next: NextFunction) => {
   if (!req.body) {
-    throw new AppError("Request body is missing", StatusCodes.BAD_REQUEST);
+    throw new AppError(`Request body is missing`, StatusCodes.BAD_REQUEST);
   }
 
-  const { name, email, password, role } = req.body as IUserPayload;
+  const { name, email, password, role } = req.body as IUserPayload;g
 
   if (!name) {
     throw new AppError(
-      `Name is required`,
+      `Name is required and type will be string/text`,
       StatusCodes.BAD_REQUEST,
     );
   }
+
+   if (typeof(name) !== "string") {
+    throw new AppError("Invalid name data type example:('hayder ali' or 'akash')", StatusCodes.BAD_REQUEST);
+   }
 
   if (name.length < 2) {
     throw new AppError(
@@ -25,23 +29,28 @@ const validateUser = (req: Request, res: Response, next: NextFunction) => {
   }
 
   // check email and email format
+  //  create only regex with Ai
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
   if (!email) {
-    throw new AppError("Email is required ", StatusCodes.BAD_REQUEST);
+    throw new AppError("Email is required and type will be string/text", StatusCodes.BAD_REQUEST);
   }
 
   if (!emailRegex.test(email)) {
-    throw new AppError("Invalid email format", StatusCodes.BAD_REQUEST);
+    throw new AppError("Invalid email format ex: (example@gmail.com)", StatusCodes.BAD_REQUEST);
   }
 
   //  check password if exist
   if (!password) {
-    throw new AppError("Password is required", StatusCodes.BAD_REQUEST);
+    throw new AppError("Password is required and type will be string", StatusCodes.BAD_REQUEST);
   }
 
+   if (typeof(password) !== "string") {
+    throw new AppError("Invalid password format type will be string example:('abc2525' or '125465')", StatusCodes.BAD_REQUEST);
+   }
+
   if (role) {
-    const validRoles = ["contributor", "maintainer"];
+    const validRoles: Roles[] = ["contributor", "maintainer"];
 
     if (!validRoles.includes(role)) {
       throw new AppError(
