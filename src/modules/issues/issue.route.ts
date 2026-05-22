fee526212651b579d73ef38ff867a4a8b9940856
hayder <hayderbd4290@gmail.com>
2026-05-23 00:01:35 +0600

    create issue update validation middleware

--- src/modules/issues/issue.route.ts
+++ src/modules/issues/issue.route.ts
@@ -5,13 +5,14 @@ import { authMiddleware } from "../../middleware/authMiddleware";
 import { roleAccess } from "../../middleware/roleAccess.middleware";
 import { USER_ROLES } from "../../types";
 import validatePostIssue from "./issue.create.validation";
+import validateUpdateIssue from "./issue.update.validation";
 
 const router = Router();
 
 router.post('/', authMiddleware(), validatePostIssue, issueController.createIssue);
 router.get("/", issueController.getAllIssues);
 router.get("/:id", issueController.getSingleIssue);
-router.patch("/:id", authMiddleware(),validatePostIssue, roleAccess(USER_ROLES.contributor, USER_ROLES.maintainer), issueController.updateIssue);
+router.patch("/:id", authMiddleware(),validateUpdateIssue, roleAccess(USER_ROLES.contributor, USER_ROLES.maintainer), issueController.updateIssue);
 
 
 export const issueRouter = router;
